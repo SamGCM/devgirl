0994504948dde4489ed5f99906cad8b0078e03f8
Samuel Cruz <samuelcruz052@gmail.com>
2021-07-12 12:22:11 -0300

    redesing do layout

--- public/scripts/menu-toggle.js
+++ public/scripts/menu-toggle.js
@@ -3,12 +3,18 @@ const menu = document.querySelector('#navbar__list')
 const navbar = document.querySelector('.navbar')
 
 
-console.log(hamburguer)
-
 hamburguer.addEventListener('click', () => {
 
-    menu.hasAttribute('class') == false ? navbar.style.setProperty('display', 'block') : navbar.style.setProperty('display', 'none')
     menu.hasAttribute('class') == false ? menu.setAttribute('class', 'toggle') : menu.removeAttribute('class')  
     menu.hasAttribute('class') == true ? menu.style.display = 'block' : menu.style.display = ''
 })
+
+
+
+
+
+
+
+
+
 
